31cdae981a2e7b8d6b091c3f7d1698455ee368b8
Eduardo Faria <eduardo21faria@gmail.com>
2024-12-20 11:35:28 +0000

    feat: basic implementation

--- src/client/Client.java
+++ src/client/Client.java
@@ -3,15 +3,15 @@ package client;
 import java.io.*;
 import java.net.Socket;
 
-import entries.GetPacket;
-import entries.PacketWrapper;
-import entries.PutPacket;
+import entries.*;
+import connection.ConnectionManager;
 
 public class Client {
-    private Socket socket;
+    private ConnectionManager conn;
+    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    public Client(Socket socket) {
-        this.socket = socket;
+    public Client(Socket socket) throws IOException {
+       this.conn = new ConnectionManager(socket);
     }
 
 
@@ -25,15 +25,25 @@ public class Client {
     // [ATENÇÃO] A implementação do método put() não está a ter em conta
     // diferentes tipos de mensagens, mais propriamente, diferentes headers.
     public void put(String key, byte[] value) throws IOException {
-        DataOutputStream out = new DataOutputStream(new BufferedOutputStream(this.socket.getOutputStream()));
-
         PutPacket putPacket = new PutPacket(key, value);
         PacketWrapper packetWrapper = new PacketWrapper(1, putPacket);
-        System.out.println(putPacket.toString());
-        packetWrapper.serialize(out);
-        out.flush();
-        // out.close(); ??
+
+        System.out.println(putPacket);
+
+        conn.send(packetWrapper);
     }
+
+    // Operação de escrita composta:
+    //
+    // void multiPut(Map<String, byte[]> pairs).
+    //
+    // Todos os pares chave-valor deverão ser atualizados / inseridos
+    // atomicamente.
+    public void multiPut(Map<String, byte[]> pairs) throws IOException {
+        for (Map.Entry e : pairs.entrySet()) 
+            put(e.getKey(), e.getValue());
+    }
+
 
 
     // Operação de leitura:
@@ -47,23 +57,154 @@ public class Client {
     // [ATENÇÃO] A implementação do método get() não está a ter em conta
     // diferentes tipos de mensagens, mais propriamente, diferentes headers.
     public byte[] get(String key) throws IOException {
-        DataOutputStream out = new DataOutputStream(new BufferedOutputStream(this.socket.getOutputStream()));
-        DataInputStream in = new DataInputStream(new BufferedInputStream(this.socket.getInputStream()));
-
         GetPacket getPacket = new GetPacket(key);
         PacketWrapper packetWrapper = new PacketWrapper(2, getPacket);
-        packetWrapper.serialize(out);
-        out.flush();
-        //out.close(); ??
 
-        PutPacket putPacket = PutPacket.deserialize(in);
-        //in.close(); ??
+        conn.send(packetWrapper);
 
+        PacketWrapper p = conn.receive();
+        PutPacket putPacket = (PutPacket) p.getPacket();
         return putPacket.getData();
     }
 
+    public void multiPut(Map<String, byte[]> pairs) throws IOException {
+        for (Map.Entry e : pairs.entrySet()) 
+            put(e.getKey(), e.getValue());
+    }
+
+    private boolean authenticate(String username, String password, int authenticationType) throws IOException {
+        AuthPacket auth = new AuthPacket(username, password);
+        PacketWrapper packetWrapper = new PacketWrapper(authenticationType, auth);
+
+        conn.send(packetWrapper);
+
+        PacketWrapper p = conn.receive();
+        AckPacket ackPacket = (AckPacket) p.getPacket();
+        return ackPacket.getAck();
+    }
+
     public void closeConnection() throws IOException {
-        this.socket.close();
+        conn.close();
+    }
+    
+    public void start() throws IOException {
+        this.loginMenu();
+    }
+
+    private void loginMenu() throws IOException {
+        boolean exited = false;
+
+        while (!exited) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Create account");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
+            System.out.print("Choose a option: ");
+
+            String choice = this.reader.readLine();
+
+            switch (choice) {
+                case "1":
+                    System.out.print("Enter username: ");
+                    String username = this.reader.readLine();
+
+                    System.out.print("Enter password: ");
+                    String password = this.reader.readLine();
+
+                    System.out.println("Creating account...");
+                    boolean accountCreated = this.authenticate(username, password, 3);
+
+                    if (accountCreated) {
+                        System.out.println("Account created successfully.");
+                    } else {
+                        System.out.println("Account creation failed.");
+                    }
+
+                    break;
+
+                case "2":
+                    System.out.print("Enter username: ");
+                    username = this.reader.readLine();
+
+                    System.out.print("Enter password: ");
+                    password = this.reader.readLine();
+
+                    System.out.println("Logging in...");
+                    boolean authenticated = this.authenticate(username, password, 4);
+
+                    // if client was able to log in, show main menu
+                    if (authenticated) {
+                        this.mainMenu();
+                    } else {
+                        System.out.println("Authentication failed.");
+                    }
+                    break;
+
+                case "3":
+                    System.out.println("Exiting...");
+                    this.closeConnection();
+                    exited = true;
+                    break;
+
+                default:
+                    System.out.println("Invalid option. Try again.");
+                    break;
+            }
+        }
+    }
+
+    private void mainMenu() throws IOException {
+        boolean exited = false;
+
+        while (!exited) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Write in the server map");
+            System.out.println("2. Request information from the server map");
+            System.out.println("3. Exit");
+            System.out.print("Choose a option: ");
+
+            String choice = this.reader.readLine();
+
+            switch (choice) {
+                case "1":
+                    System.out.print("Enter key: ");
+                    String key = this.reader.readLine();
+
+                    System.out.print("Enter data: ");
+                    String data = this.reader.readLine();
+
+                    byte[] value = data.getBytes();
+
+                    System.out.println("Asking server to store key: " + key + " and data: " + data);
+                    this.put(key, value);
+                    break;
+
+                case "2":
+                    System.out.print("Enter key to fetch data: ");
+                    String fetchKey = this.reader.readLine();
+
+                    System.out.println("Asking server for data corresponding to key: " + fetchKey);
+                    byte[] dataFromServer = this.get(fetchKey);
+
+                    if (dataFromServer != null && dataFromServer.length > 0) {
+                        System.out.print("Data received: ");
+                        System.out.println(new String(dataFromServer));
+                    } else {
+                        System.out.println("No data found for the given key.");
+                    }
+                    break;
+
+                case "3":
+                    System.out.println("Exiting...");
+                    exited = true;
+                    break;
+
+                default:
+                    System.out.println("Invalid option. Try again.");
+                    break;
+            }
+        }
     }
 }
+
 
