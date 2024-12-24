1a4e96d688635078e6c970d1c76e11f3a88da740
Hélder Gomes <116376171+helderrrg@users.noreply.github.com>
2024-12-24 15:20:57 +0000

    feat: multithreaded client (#33)
    * Client methods locked
    
    * feat: adapt to LogWriter
    
    * fix: Reduced thread wake-ups
    
    ---------
    
    Co-authored-by: Pedro Pereira <pfpereira2004@gmail.com>

---
+++ src/utils/LogWriter.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LogWriter {
+    private static final String LOG_FILE = "log.txt";
+    private static final Lock lock = new ReentrantLock();
+
+    public void println(String message) {
+        lock.lock();
+        try (PrintWriter writer = new PrintWriter(new FileWriter(new File(LOG_FILE), true))) {
+            writer.println("--------------------------------------------------");
+            writer.println("Log at " + new java.text.SimpleDateFormat("dd/MM/yyyy - HH:mm:ss").format(new java.util.Date()));
+            writer.println(message);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally { 
+            lock.unlock();
+        }
+    }
+}
