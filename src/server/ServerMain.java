8e2a08fc265ffad9091e0c0e6806517f69b80e4d
helderrrg <heldergomes.1003@gmail.com>
2024-12-26 20:44:18 +0000

    feat: add thread pooling

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -6,19 +6,23 @@ import java.net.Socket;
 
 public class ServerMain {
     public static void main(String[] args) {
-        if (args.length != 2 && args.length != 3) {
-            System.out.println("Usage: java ServerMain <MAX_CONCURRENT_USERS> <FILE_PATH> ['--load']");
+        if (args.length != 3 && args.length != 4) {
+            System.out.println("Usage: java ServerMain <THREADPOOL_SIZE> <MAX_CONCURRENT_USERS> <FILE_PATH> ['--load']");
             return;
         }
 
-        int S = Integer.parseInt(args[0]);
-        String filePath = args[1];
+        int threadPoolSize = Integer.parseInt(args[0]);
+        int S = Integer.parseInt(args[1]);
+        String filePath = args[2];
 
         // Create an empty server instance and read the state from the file
         Server server = new Server();
-        if (args.length == 3 && args[2].equals("--load")) server = Server.readState(filePath); 
+        if (args.length == 4 && args[3].equals("--load")) server = Server.readState(filePath); 
         server.setS(S);
         server.running();
+
+        // Create a ThreadPool with a fixed number of threads
+        ThreadPool threadPool = new ThreadPool(threadPoolSize);
 
         try (ServerSocket ss = new ServerSocket(8080)) {
             System.out.println("Server starting...");
@@ -27,8 +31,8 @@ public class ServerMain {
                 Socket socket = ss.accept();
                 if (!server.isRunning()) break; // check if the server is still running
 
-                Thread thread = new Thread(new ServerHandler(socket, server)); // Pass server instance
-                thread.start();
+                // Submit the ServerHandler task to the ThreadPool
+                threadPool.submitTask(new ServerHandler(socket, server));
             }
         } catch (IOException e) {
             e.printStackTrace();
